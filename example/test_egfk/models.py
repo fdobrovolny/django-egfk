130ecba729d88c44a6a29cac136979c8b4773ed1
Filip Dobrovolny <fdobrovo@redhat.com>
2016-08-27 01:02:14 +0200

    EXAMPLE: Fixed reference. Added models for content_type as poperty

--- example/test_egfk/models.py
+++ example/test_egfk/models.py
@@ -3,33 +3,46 @@ from __future__ import unicode_literals
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-from django_egfk.fields import GenericForeignKey
+from django_egfk.fields import EnhancedGenericForeignKey
+
+
+class TestSampleModel(models.Model):
+    """You can use this model to test GenericForeignKey."""
+    text = models.TextField()
+
+"""
+Basic GenericForeignKey on another model connected via ForeignKey.
+Child (object_id) -(fk) > Main(content_type)
+  |
+(egfk)
+  V
+Some Model
+"""
 
 
 class Main(models.Model):
-    text = models.TextField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, blank=True, null=True)
 
 
 class Child(models.Model):
     object_id = models.PositiveIntegerField()
     main = models.ForeignKey(Main, related_name="child")
-    content_object = GenericForeignKey('main.content_type', 'object_id')
+    content_object = EnhancedGenericForeignKey('main.content_type', 'object_id')
 
 
 """
- Multiple GenericForeignKey TestCase
+Multiple nestead GenericForeignKey TestCase
 A (egfk)-> B (D ContentType HERE)
 ^
 | (fk)
-C(D ID HERE) (egfk)-> D
+C(D ID HERE) (egfk)-> D(TestSampleModel)
 """
 
 
 class A(models.Model):
     content_type_B = models.ForeignKey(ContentType, on_delete=models.CASCADE, blank=True, null=True)
     object_id_B = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type_B', 'object_id_B')
+    content_object = EnhancedGenericForeignKey('content_type_B', 'object_id_B')
 
 
 class B(models.Model):
@@ -39,8 +52,23 @@ class B(models.Model):
 class C(models.Model):
     A = models.ForeignKey(A)
     object_id_D = models.PositiveIntegerField(blank=True, null=True)
-    content_object_D = GenericForeignKey('A.content_object.content_type_D', 'object_id_D')
+    content_object_D = EnhancedGenericForeignKey('A.content_object.content_type_D', 'object_id_D')
 
 
-class D(models.Model):
+"""
+Field can be property on model.
+"""
+
+
+class Alpha(models.Model):
     text = models.TextField()
+
+    @property
+    def content_type(self):
+        return ContentType.objects.get_for_model(TestSampleModel)
+
+
+class Beta(models.Model):
+    alpha = models.ForeignKey(Alpha)
+    object_id = models.PositiveIntegerField()
+    content_object = EnhancedGenericForeignKey('alpha.content_type', 'object_id')
